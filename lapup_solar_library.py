99b19197bc2dae837d0d29e5e72e77d462287102
thanos <athanarg@gmail.com>
2021-01-06 20:41:58 +0200

    Version 1.0 - all functions valdiated

--- lapup_solar_library.py
+++ lapup_solar_library.py
@@ -1,12 +1,29 @@
 """
-Functions for use in solar radiation calculations.
+Functions for use in solar radiation calculations based on a datetime_object:
+Date and time YYYY-MM-DD h:m:s (local standard time without the daylight savings shift)
+
+Attention: UTC time stamped measurements must be converted to local standard time.
+Example:
+    Assuming that the data form a Pandas Dataframe object with a date - time timestamp in UTC, the conversion is made
+    as follows:
+
+    pandas_dataframe.index = df.index + pd.Timedelta(hours=2)  # Converts timestamp from UTC to Greek local time
 
 Modules:
 
-libraries
-import numpy as np
+decl: solar declination angle (rad)
+eqtime: equation of time (minutes)
+fractional_year: fractional year (rad)
+hour_angle: solar hour angle (rad)
+sazimuth: solar azimuth angle (rad)
+sza: solar zenith angle (rad)
+true_solar_time: true solar time as python datetime object YYYY-MM-DD h:m:s
 
-A. Argiriou, LAPUP, Dec 2020
+libraries used: numpy as np
+
+Version 1.0
+
+A. Argiriou, LAPUP, University of Patras, 2020-01-06
 """
 
 import pandas as pd
@@ -16,31 +33,33 @@ import numpy as np
 def decl(datetime_object):
     """
     Calculates the solar declination
-    :param: datetime_object - Date and (local standard) time YYYY-MM-DD h:m:s (without the daylight savings shift)
-    :returns: solar declination angle (rad)
+    :param: datetime_object: Date and time YYYY-MM-DD h:m:s (local standard time without the daylight savings shift)
+    :return: solar declination angle (rad)
     """
 
-    return 0.006918 - 0.399912 * np.cos(gamma(datetime_object)) + 0.070257 * np.sin(gamma(datetime_object)) - 0.006758 \
-           * np.cos(2 * gamma(datetime_object)) + 0.000907 * np.sin(2 * gamma(datetime_object)) - 0.002697 * \
-           np.cos(3 * gamma(datetime_object)) + 0.00148 * np.sin(3 * gamma(datetime_object))
+    return 0.006918 - 0.399912 * np.cos(fractional_year(datetime_object)) + 0.070257 * np.sin(fractional_year
+            (datetime_object)) - 0.006758 * np.cos(2 * fractional_year(datetime_object)) + 0.000907 * \
+            np.sin(2 * fractional_year(datetime_object)) - 0.002697 * np.cos(3 * fractional_year(datetime_object)) +\
+           0.00148 * np.sin(3 * fractional_year(datetime_object))
 
 
 def eqtime(datetime_object):
     """
     Calculates the equation of time
-    :param datetime_object: Date and (local standard) time YYYY-MM-DD h:m:s (without the daylight savings shift)
-    :returns: equation of time (eqtime) (minutes)
+    :param datetime_object: Date and time YYYY-MM-DD h:m:s (local standard time without the daylight savings shift)
+    :return: equation of time (minutes)
     """
 
-    return 229.18 * (0.000075 + 0.001868 * np.cos(gamma(datetime_object)) - 0.032077 * np.sin(gamma(datetime_object))
-                     - 0.014615 * np.cos(2 * gamma(datetime_object)) - 0.040849 * np.sin(2 * gamma(datetime_object)))
+    return 229.18 * (0.000075 + 0.001868 * np.cos(fractional_year(datetime_object)) - 0.032077 * np.sin(fractional_year
+            (datetime_object)) - 0.014615 * np.cos(2 * fractional_year(datetime_object)) - 0.040849 *
+                     np.sin(2 * fractional_year(datetime_object)))
 
 
-def gamma(datetime_object):
+def fractional_year(datetime_object):
     """
     Calculates the fractional year corresponding to a datetime object
-    :param datetime_object: Date and (local standard) time YYYY-MM-DD h:m:s (without the daylight savings shift)
-    :return: fractional year (in radians)
+    :param datetime_object: Date and time YYYY-MM-DD h:m:s (local standard time without the daylight savings shift)
+    :return: fractional year (radians)
     """
     return 2 * np.pi * (datetime_object.dayofyear - 1 + (datetime_object.hour - 12) / 24) / 365
 
@@ -48,7 +67,7 @@ def gamma(datetime_object):
 def true_solar_time(datetime_object, longitude):
     """
     Calculates the true solar time (TST)
-    :param datetime_object: Date and time YYYY-MM-DD h:m:s (without the daylight savings shift)
+    :param datetime_object: Date and time YYYY-MM-DD h:m:s (local standard time without the daylight savings shift)
     :param longitude: longitude at the position of interest in degrees (positive to the east of the Prime Meridian)
     :return: true solar time as python datetime object YYYY-MM-DD h:m:s
     """
@@ -56,6 +75,7 @@ def true_solar_time(datetime_object, longitude):
     timezone = round(longitude / 15) + 1
 
     time_offset = eqtime(datetime_object) + 4. * longitude - 60. * timezone
+    # time_offset = eqtime(datetime_object) + 4. * (30 - longitude)
 
     return datetime_object + pd.Timedelta(minutes=time_offset)
 
@@ -63,57 +83,56 @@ def true_solar_time(datetime_object, longitude):
 def hour_angle(datetime_object, longitude):
     """
     Calculates the solar hour angle
-    :param datetime_object: Date and time YYYY-MM-DD h:m:s (without the daylight savings shift)
+    :param datetime_object: Date and time YYYY-MM-DD h:m:s (local standard time without the daylight savings shift)
     :param longitude: longitude at the position of interest in degrees (positive to the east of the Prime Meridian)
-    :return: solar hour angle (deg)
+    :return: solar hour angle (rad)
     """
 
     tst = true_solar_time(datetime_object, longitude)
-    tst_min = tst.hour * 60. + tst.minute + (tst.second + tst.microsecond * 10 ** -6) / 60.
-    return tst_min / 4. - 180.
+    tst_decimal = tst.hour + (tst.minute + (tst.second + tst.microsecond * 10 ** -6) / 60.) / 60.
+    return 15. * (tst_decimal - 12.) * np.pi / 180.
 
 
-def cos_sza(datetime_object, longitude, latitude):
+def sza(datetime_object, latitude, longitude):
     """
-    Calculates the cosine of the solar zenith angle
-    :param datetime_object: datetime_object: Date and time YYYY-MM-DD h:m:s (without the daylight savings shift)
+    Calculates the solar zenith angle
+    :param datetime_object: Date and time YYYY-MM-DD h:m:s (local standard time without the daylight savings shift)
     :param longitude:longitude at the position of interest in degrees (positive to the east of the Prime Meridian)
     :param latitude: latitude at the position of interest in degrees (positive to the north of the equator)
-    :return: the cosine of the solar zenith angle
+    :return: solar zenith angle (rad)
     """
 
-    return np.sin(latitude * np.pi / 180.) * np.sin(decl(datetime_object)) + np.cos(latitude * np.pi / 180.) * \
-           np.cos(decl(datetime_object)) * np.cos(hour_angle(datetime_object, longitude) * np.pi / 180.)
+    return np.arccos(np.sin(latitude * np.pi / 180.) * np.sin(decl(datetime_object)) + np.cos(latitude * np.pi / 180.) *
+                     np.cos(decl(datetime_object)) * np.cos(hour_angle(datetime_object, longitude)))
 
 
-def sazimuth(datetime_object, longitude, latitude):
+def sazimuth(datetime_object, latitude, longitude):
     """
     Calculates the solar azimuth angle
-    :param datetime_object: datetime_object: Date and time YYYY-MM-DD h:m:s (without the daylight savings shift)
+    :param datetime_object: Date and time YYYY-MM-DD h:m:s (local standard time without the daylight savings shift)
     :param longitude:longitude at the position of interest in degrees (positive to the east of the Prime Meridian)
     :param latitude: latitude at the position of interest in degrees (positive to the north of the equator)
     :return: solar azimuth angle (rad)
     """
-    if hour_angle(datetime_object, longitude) >= 0:
-        sign = 1
-    else:
-        sign = -1
+    d2r = np.pi / 180.  # Converts degrees to rads
+    return 2 * np.pi - np.arccos((np.sin(decl(datetime_object)) * np.cos(latitude * d2r) - np.cos(hour_angle(
+        datetime_object, longitude)) * np.cos(decl(datetime_object)) * np.sin(latitude * d2r)) /
+                     np.sin(sza(datetime_object, latitude, longitude)))
 
-    return sign * np.abs(np.arccos(np.sin(latitude * np.pi / 180.) * cos_sza(datetime_object, longitude, latitude) -
-                                   np.sin(decl(datetime_object))) / np.cos(latitude * np.pi / 180.) /
-                         np.sin(np.arccos(cos_sza(datetime_object, longitude, latitude))))
+# Example of application on solar data from the LAPUP radiometric station in Patras
 
 
-df = pd.read_csv('Solar_1min_2019.txt', usecols=[0, 6], index_col=0, parse_dates=True, sep=',', header=None)
-
-df.index = df.index + pd.Timedelta(hours=2)  # Converts timestamp from UTC to local time
-
-lat = 38.29138889
-lon = 21.78861111
-
-#  my_time = df.index[680]
-#  my_solar_time = true_solar_time(my_time, longitude)
-
-for i in range(620, 689):
-    print(df.index[i])
-    print(sazimuth(df.index[i], lon, lat)*180/np.pi)
+# df = pd.read_csv('Solar_1min_2019.txt', usecols=[0, 6], index_col=0, parse_dates=True, sep=',', header=None)
+#
+# df.index = df.index + pd.Timedelta(hours=2)  # Converts timestamp from UTC to local time
+#
+# # Latitude and longitude in decimal form
+#
+# lat = 38.29138889
+# lon = 21.78861111
+#
+# for i in [680, 250000]:
+#     print(df.index[i])
+#     print(hour_angle(df.index[i], lon))
+#     print(90 - sza(df.index[i], lat, lon) * 180/np.pi)
+#     print(sazimuth(df.index[i], lat, lon)*180./np.pi)
