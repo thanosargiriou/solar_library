12d61eef610cccf54e8855cf19a2acf222357bce
thanos <athanarg@gmail.com>
2021-01-09 14:09:31 +0200

    Version 1.02

--- lapup_solar_library.py
+++ lapup_solar_library.py
@@ -11,27 +11,30 @@ Example:
 
 Modules:
 
-decl: solar declination angle (rad)
-eqtime: equation of time (minutes)
+declination: solar declination angle (rad)
+equation_of_time: equation of time (minutes)
 fractional_year: fractional year (rad)
 hour_angle: solar hour angle (rad)
-sazimuth: solar azimuth angle (rad)
+saz: solar azimuth angle (rad)
 sza: solar zenith angle (rad)
 true_solar_time: true solar time as python datetime object YYYY-MM-DD h:m:s
 
 libraries used: numpy as np
 
-Version 1.01
+Version 1.01 2020-01-08
 Intro of numpy.radians() function
 
-A. Argiriou, LAPUP, University of Patras, 2020-01-08
+Version 1.02 2020-01-09
+Change of some names
+
+A. Argiriou, LAPUP, University of Patras
 """
 
 import pandas as pd
 import numpy as np
 
 
-def decl(datetime_object):
+def declination(datetime_object):
     """
     Calculates the solar declination
     :param: datetime_object: Date and time YYYY-MM-DD h:m:s (local standard time without the daylight savings shift)
@@ -44,7 +47,7 @@ def decl(datetime_object):
                                                                  0.00148 * np.sin(3 * fractional_year(datetime_object))
 
 
-def eqtime(datetime_object):
+def equation_of_time(datetime_object):
     """
     Calculates the equation of time
     :param datetime_object: Date and time YYYY-MM-DD h:m:s (local standard time without the daylight savings shift)
@@ -75,7 +78,7 @@ def true_solar_time(datetime_object, longitude):
 
     timezone = round(longitude / 15) + 1
 
-    time_offset = eqtime(datetime_object) + 4. * longitude - 60. * timezone
+    time_offset = equation_of_time(datetime_object) + 4. * longitude - 60. * timezone
 
     return datetime_object + pd.Timedelta(minutes=time_offset)
 
@@ -103,11 +106,11 @@ def sza(datetime_object, latitude, longitude):
     :return: solar zenith angle (rad)
     """
 
-    return np.arccos(np.sin(np.radians(latitude)) * np.sin(decl(datetime_object)) + np.cos(np.radians(latitude)) *
-                     np.cos(decl(datetime_object)) * np.cos(hour_angle(datetime_object, longitude)))
+    return np.arccos(np.sin(np.radians(latitude)) * np.sin(declination(datetime_object)) + np.cos(np.radians(latitude))
+                     * np.cos(declination(datetime_object)) * np.cos(hour_angle(datetime_object, longitude)))
 
 
-def sazimuth(datetime_object, latitude, longitude):
+def saz(datetime_object, latitude, longitude):
     """
     Calculates the solar azimuth angle
     :param datetime_object: Date and time YYYY-MM-DD h:m:s (local standard time without the daylight savings shift)
@@ -116,24 +119,24 @@ def sazimuth(datetime_object, latitude, longitude):
     :return: solar azimuth angle (rad)
     """
 
-    return 2 * np.pi - np.arccos((np.sin(decl(datetime_object)) * np.cos(np.radians(latitude)) - np.cos(hour_angle(
-        datetime_object, longitude)) * np.cos(decl(datetime_object)) * np.sin(np.radians(latitude))) /
-                     np.sin(sza(datetime_object, latitude, longitude)))
+    return 2 * np.pi - np.arccos((np.sin(declination(datetime_object)) * np.cos(np.radians(latitude)) -
+                                  np.cos(hour_angle(datetime_object, longitude)) * np.cos(declination(datetime_object))
+                                 * np.sin(np.radians(latitude))) / np.sin(sza(datetime_object, latitude, longitude)))
 
 # Example of application on solar data from the LAPUP radiometric station in Patras
 
 
-# df = pd.read_csv('Solar_1min_2019.txt', usecols=[0, 6], index_col=0, parse_dates=True, sep=',', header=None)
-#
-# df.index = df.index + pd.Timedelta(hours=2)  # Converts timestamp from UTC to local time
-#
-# # Latitude and longitude in decimal form
-#
-# lat = 38.29138889
-# lon = 21.78861111
-#
-# for i in [680, 250000]:
-#     print(df.index[i])
-#     print(hour_angle(df.index[i], lon))
-#     print(90 - np.rad2deg(sza(df.index[i], lat, lon)))
-#     print(np.rad2deg(sazimuth(df.index[i], lat, lon)))
+df = pd.read_csv('Solar_1min_2019.txt', usecols=[0, 6], index_col=0, parse_dates=True, sep=',', header=None)
+
+df.index = df.index + pd.Timedelta(hours=2)  # Converts timestamp from UTC to local time
+
+# Latitude and longitude in decimal form
+
+lat = 38.29138889
+lon = 21.78861111
+
+for i in [680, 250000]:
+    print(df.index[i])
+    print(hour_angle(df.index[i], lon))
+    print(90 - np.rad2deg(sza(df.index[i], lat, lon)))
+    print(np.rad2deg(saz(df.index[i], lat, lon)))
